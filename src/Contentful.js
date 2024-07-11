be3597f2114be40bad0fa0e63d18a79656ff4884
princetomer <115070361+princetomer@users.noreply.github.com>
2024-07-11 14:19:58 +0530

    Initial commit

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,15 @@
+// const contentful = require("contentful");
+
+// export default contentful.createClient({
+//   space: process.env.REACT_APP_API_SPACE,
+//   accessToken: process.env.REACT_APP_ACCESS_TOKEN
+// });
+
+
 const contentful = require("contentful");
 
 export default contentful.createClient({
   space: process.env.REACT_APP_API_SPACE,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
+
